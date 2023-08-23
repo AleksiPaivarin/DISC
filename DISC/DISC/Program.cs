5ce39700dad8a3fb49df728d2fd03a13fcd0a4b2
Aleksi <Aleksi@DESKTOP-GR3N2GU>
2023-08-23 16:16:05 +0300

    lisätty html pohja

--- DISC/DISC/Program.cs
+++ DISC/DISC/Program.cs
@@ -1,7 +1,26 @@
 
 using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string apiKey = "2fe7354686ed4b60834bc601bc7dc012";
+        BungieApiClient bungieApiClient = new BungieApiClient(apiKey);
 
+        string playerName = "desiredPlayerName";
+        var playerInfo = await bungieApiClient.GetPlayerInfoAsync(playerName);
+        Console.WriteLine(playerInfo); // or display it as needed
+
+        // Add more code as necessary
+
+        // This ensures the console application doesn't exit immediately
+        Console.ReadLine();
+    }
+}
 public class BungieApiClient
+
 {
     private const string API = "2fe7354686ed4b60834bc601bc7dc012";
     private readonly HttpClient _httpClient;
@@ -9,12 +28,13 @@ public class BungieApiClient
     public BungieApiClient(string apiKey)
     {
         _httpClient = new HttpClient();
-        _httpClient.DefaultRequestHeaders.Add(API, apiKey);
+        _httpClient.DefaultRequestHeaders.Add("2fe7354686ed4b60834bc601bc7dc012", apiKey);
     }
+
 
     public async Task<string?> GetPlayerInfoAsync(string playerName)
     {
-        string apiUrl = $"https://www.bungie.net/Platform{playerName}/";
+        string apiUrl = $"https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/{playerName}/";
         HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
         if (response.IsSuccessStatusCode)
         {
@@ -27,16 +47,18 @@ public class BungieApiClient
 
 
 
-    public class PlayerResponse
+public class PlayerResponse
 {
     [JsonProperty("Response")]
-    public required List<PlayerData> Players { get; set; }
-    public class PlayerData
-    {
-        [JsonProperty("membershipId")]
-        public required string MembershipId { get; set; }
-
-        // Add other properties as needed
-    }
+    public List<PlayerData> Players { get; set; }
 }
+
+public class PlayerData
+{
+    [JsonProperty("membershipId")]
+    public string MembershipId { get; set; }
+
+    // Add other properties as needed
+}
+
 
