e568651a91db8ed0bb5c0a5e9f895e9128bb186f
Aleksi <Aleksi@DESKTOP-GR3N2GU>
2023-08-23 15:35:39 +0300

    Lisätty koodi API-lukuun

--- DISC/DISC/Program.cs
+++ DISC/DISC/Program.cs
@@ -1,6 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Newtonsoft.Json;
 
-app.Run();
+public class BungieApiClient
+{
+    private const string API = "2fe7354686ed4b60834bc601bc7dc012";
+    private readonly HttpClient _httpClient;
+
+    public BungieApiClient(string apiKey)
+    {
+        _httpClient = new HttpClient();
+        _httpClient.DefaultRequestHeaders.Add(API, apiKey);
+    }
+
+    public async Task<string?> GetPlayerInfoAsync(string playerName)
+    {
+        string apiUrl = $"https://www.bungie.net/Platform{playerName}/";
+        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        return null;
+    }
+}
+
+
+
+
+    public class PlayerResponse
+{
+    [JsonProperty("Response")]
+    public required List<PlayerData> Players { get; set; }
+    public class PlayerData
+    {
+        [JsonProperty("membershipId")]
+        public required string MembershipId { get; set; }
+
+        // Add other properties as needed
+    }
+}
+
